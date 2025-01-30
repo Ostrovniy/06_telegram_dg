5f9a95f7627d13273a061e7ab7e5715aba73b4f8
Ostrovniy <ostrovniy0001@gmail.com>
2025-01-30 15:32:14 +0200

    add branch dev and test file

--- main.py
+++ main.py
@@ -19,31 +19,17 @@ TOKEN = os.getenv("BOT_TOKEN", "").strip('"')  # Удаляем возможны
 if not TOKEN:
     raise ValueError("BOT_TOKEN is not set or is empty!")
 
-# All handlers should be attached to the Router (or Dispatcher)
 
 dp = Dispatcher()
 
 
 @dp.message(CommandStart())
 async def command_start_handler(message: Message) -> None:
-    """
-    This handler receives messages with `/start` command
-    """
-    # Most event objects have aliases for API methods that can be called in events' context
-    # For example if you want to answer to incoming message you can use `message.answer(...)` alias
-    # and the target chat will be passed to :ref:`aiogram.methods.send_message.SendMessage`
-    # method automatically or call API method directly via
-    # Bot instance: `bot.send_message(chat_id=message.chat.id, ...)`
     await message.answer(f"Hello, {html.bold(message.from_user.full_name)}!, Накрывай столы!!!228")
 
 
 @dp.message()
 async def echo_handler(message: Message) -> None:
-    """
-    Handler will forward receive a message back to the sender
-
-    By default, message handler will handle all message types (like a text, photo, sticker etc.)
-    """
     try:
         # Send a copy of the received message
         await message.send_copy(chat_id=message.chat.id)
